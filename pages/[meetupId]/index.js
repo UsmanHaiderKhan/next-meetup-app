351c12e78194221986fa1de70e8714f728a15830
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-10-19 14:28:21 +0200

    🎉 Add : MeetupDetails page with static MeetupDetail
    Created a new dynamic page at [meetupId]/index.js that renders the MeetupDetail component with static props for image, title, address, and description.

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+    return (
+        <MeetupDetail
+            image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg"
+            title="First Meetup" address="Some where in the world"
+            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum placeat temporibus voluptate. Atque debitis eaque incidunt ipsa, ipsam maxime minima nulla sapiente, sequi sunt suscipit tempore vel veniam veritatis voluptatibus!"/>
+
+    );
+}
+export default MeetupDetails;
