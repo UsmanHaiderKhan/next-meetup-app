5745fe24e0855e56a84e05c4aa443952a3d3c389
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-10-21 11:23:03 +0200

    ♻️Update : Add getStaticPaths and getStaticProps to meetup page
    Implemented getStaticPaths to define static paths for meetups and getStaticProps to provide meetup data as props. This enables static generation for individual meetup detail pages.

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -9,4 +9,39 @@ function MeetupDetails() {
 
     );
 }
+
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: 'm1',
+                },
+            },
+            {
+                params: {
+                    meetupId: 'm2',
+                }
+            },
+        ]
+    }
+
+}
+
+export async function getStaticProps(context) {
+    const meetupId = context.params.meetupId;
+    return {
+        props: {
+            meetupData: {
+                id: meetupId,
+                title: 'First Meetup',
+                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+                address: 'Some where in the world',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum placeat temporibus voluptate. Atque debitis eaque incidunt ipsa, ipsam maxime minima nulla sapiente, sequi sunt suscipit tempore vel veniam veritatis voluptatibus!'
+            },
+        }
+    }
+}
+
 export default MeetupDetails;
