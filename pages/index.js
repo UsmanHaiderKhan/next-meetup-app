82258261b13dc9af92bba9e575e33bf53cbcc35a
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-10-21 11:23:34 +0200

    ♻️Update : add getServerSideProps in index page
    Replaces getStaticProps with getServerSideProps for dynamic server-side rendering of meetups data. Removes unused imports and comments out the previous static props implementation.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import MeetupList from "../components/meetups/MeetupList";
-import {useEffect, useState} from "react";
 
 const DUMMY_MEETUPS = [
     {
@@ -25,16 +24,28 @@ const DUMMY_MEETUPS = [
 // }, []);
 
 
-function HomePage(props){
-    return(
-        <MeetupList meetups={props.meetups} />
+function HomePage(props) {
+    return (
+        <MeetupList meetups={props.meetups}/>
     )
 }
-export async function getStaticProps(){
+
+export async function getServerSideProps(context) {
+    const req = context.req;
+    const res = context.res;
     return {
         props: {
             meetups: DUMMY_MEETUPS
         }
     }
 }
+
+// export async function getStaticProps(){
+//     return {
+//         props: {
+//             meetups: DUMMY_MEETUPS
+//         },
+//         revalidate: 10
+//     }
+// }
 export default HomePage;
