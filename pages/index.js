119d559ad76fbf5915a2bca7a5ccd8d2a787ad1c
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-10-22 23:00:46 +0200

    ♻️ Update : Fetch meetups data from MongoDB in getStaticProps
    Replaced the use of dummy data with a MongoDB query in getStaticProps to fetch meetups dynamically. The server-side data fetching method was commented out, and the static props function now connects to the database, retrieves meetups, and passes them as props to the page.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import MeetupList from "../components/meetups/MeetupList";
+import {MongoClient} from "mongodb";
 
 const DUMMY_MEETUPS = [
     {
@@ -30,22 +31,32 @@ function HomePage(props) {
     )
 }
 
-export async function getServerSideProps(context) {
-    const req = context.req;
-    const res = context.res;
-    return {
-        props: {
-            meetups: DUMMY_MEETUPS
-        }
-    }
-}
-
-// export async function getStaticProps(){
+// export async function getServerSideProps(context) {
+//     const req = context.req;
+//     const res = context.res;
 //     return {
 //         props: {
 //             meetups: DUMMY_MEETUPS
-//         },
-//         revalidate: 10
+//         }
 //     }
 // }
+
+export async function getStaticProps(){
+    const client = await MongoClient.connect('mongodb+srv://uk47:Hm6gWspVAs98NCXM@cluster0.8r87qik.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0');
+    const db = client.db();
+    const meetupsCollection = db.collection('meetups');
+    const meetups = await meetupsCollection.find().toArray();
+    await client.close();
+    return {
+        props: {
+            meetups: meetups.map(meetup => ({
+                title: meetup.title,
+                address: meetup.address,
+                image: meetup.image,
+                id: meetup._id.toString(),
+            })),
+        },
+        revalidate: 10
+    }
+}
 export default HomePage;
