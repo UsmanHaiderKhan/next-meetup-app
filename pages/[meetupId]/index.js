d42f97d4e201a86d3e1f664d918530a12c7175a8
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-10-23 22:00:36 +0200

    ♻️Update : Add SEO meta tags and enable ISR for meetup page
    Wrapped MeetupDetails in Fragment and Head to set dynamic title and meta description for SEO. Changed getStaticPaths fallback to 'blocking' and enabled incremental static regeneration with revalidate in getStaticProps.

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,15 +1,21 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 import {MongoClient, ObjectId} from "mongodb";
+import {Fragment} from "react";
+import Head from "next/head";
 
 function MeetupDetails(props) {
     return (
-        <MeetupDetail
-            image={props.meetupData.image}
-            title={props.meetupData.title}
-            description={props.meetupData.description}
-            address={props.meetupData.address}
-        />
-
+        <Fragment>
+            <Head>
+                <title>{props.meetupData.title}</title>
+                <meta name="description" content={props.meetupData.description}/>
+            </Head>
+            <MeetupDetail
+                image={props.meetupData.image}
+                title={props.meetupData.title}
+                description={props.meetupData.description}
+                address={props.meetupData.address}/>
+        </Fragment>
     );
 }
 
@@ -17,10 +23,10 @@ export async function getStaticPaths() {
     const client = await MongoClient.connect('mongodb+srv://uk47:Hm6gWspVAs98NCXM@cluster0.8r87qik.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0');
     const db = client.db();
     const meetupsCollection = db.collection('meetups');
-    const meetups= await meetupsCollection.find({}, {_id: 1}).toArray();
+    const meetups = await meetupsCollection.find({}, {_id: 1}).toArray();
     await client.close();
     return {
-        fallback: false,
+        fallback: 'blocking',
         paths: meetups.map(meetup => ({
             params: {meetupId: meetup._id.toString()}
         }))
@@ -35,7 +41,7 @@ export async function getStaticProps(context) {
     const client = await MongoClient.connect('mongodb+srv://uk47:Hm6gWspVAs98NCXM@cluster0.8r87qik.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0');
     const db = client.db();
     const meetupsCollection = db.collection('meetups');
-    const selectedMeetups= await meetupsCollection.findOne({_id: new ObjectId(meetupId)});
+    const selectedMeetups = await meetupsCollection.findOne({_id: new ObjectId(meetupId)});
     await client.close();
     return {
         props: {
@@ -46,7 +52,8 @@ export async function getStaticProps(context) {
                 image: selectedMeetups.image,
                 description: selectedMeetups.description,
             }
-        }
+        },
+        revalidate: 1,
     }
 }
 
