a9310fe40fa24fa5de89004f2bd90100fafb005c
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-10-24 21:26:20 +0200

    ♻️Update : Add Head metadata to NewMeetupPage
    Wrapped NewMeetupPage in a Fragment and added Head component to set the page title and meta description for better SEO and user experience.

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,9 +1,12 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 import {useRouter} from "next/router";
+import {Fragment} from "react";
+import Head from "next/head";
 
 function NewMeetupPage() {
     const router = useRouter();
-   async function addMeetupHandler(meetupData) {
+
+    async function addMeetupHandler(meetupData) {
         const response = await fetch('/api/new-meetup', {
             method: 'POST',
             body: JSON.stringify(meetupData),
@@ -11,12 +14,19 @@ function NewMeetupPage() {
                 'Content-Type': 'application/json'
             }
         });
-        const data =  response.json();
-        console.log(data);
+        const data = response.json();
         router.push('/');
     }
+
     return (
-        <NewMeetupForm onAddMeetup={addMeetupHandler} />
+        <Fragment>
+            <Head>
+                <title>Add New Meetup</title>
+                <meta name="description" content="Add your own meetups and create amazing networking opportunities."/>
+            </Head>
+            <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+        </Fragment>
     )
 }
+
 export default NewMeetupPage;
